3b83a85566299b82b3f4badd52a75f9d2b0619dd
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-14 18:34:51 +0530

    add login route

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,7 +2,7 @@ import { connect } from "@/dbConfig/dgConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
-import { error } from "console";
+
 
 connect();
 
