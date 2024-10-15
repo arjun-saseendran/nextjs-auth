e485b931205aaddf5df47870bebda0a80b8efa9f
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-15 21:51:01 +0530

    add transporter

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -22,7 +22,16 @@ export const sendEmail = async({email, emailType, userId}: any) => {
 
         }
 
-        
+        const transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+  port: 587,
+  secure: false, // true for port 465, false for other ports
+  auth: {
+    user: "maddison53@ethereal.email",
+    pass: "jn7jnAPss4f63QBp6D",
+
+        }
+    })
         
     } catch (error: any) {
         throw new Error(error.message)
