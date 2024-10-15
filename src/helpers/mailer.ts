90b37bdeb772fac41afd6c6f02ab1c09f5b1c76d
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-15 22:01:42 +0530

    add mailOptions

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,41 +1,46 @@
-import nodemailer from 'nodemailer';
-import User from '@/models/userModel'
-import bcryptjs from 'bcryptjs'
+import nodemailer from "nodemailer";
+import User from "@/models/userModel";
+import bcryptjs from "bcryptjs";
 
+export const sendEmail = async ({ email, emailType, userId }: any) => {
+  try {
+    //create a hashed token
+    const hashedToken = await bcryptjs.hash(userId.toString(), 10);
 
-export const sendEmail = async({email, emailType, userId}: any) => {
-    try {
-
-        //create a hashed token
-        const hashedToken = await bcryptjs.hash(userId.toString(), 10)
-
-        if(emailType === 'VERIFY'){
-            await User.findByIdAndUpdate(userId, {
-              verifyToken: hashedToken,
-              verifyTokenExpiry: Date.now() + 3600000,
-            });
-        }else if(emailType === 'RESET'){
-            await User.findByIdAndUpdate(userId, {
-              forgotPasswordToken: hashedToken,
-              forgotPasswordTokenExpiry: Date.now() + 3600000,
-            });
-
-        }
-
-        const transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-  port: 587,
-  secure: false, // true for port 465, false for other ports
-  auth: {
-    user: "maddison53@ethereal.email",
-    pass: "jn7jnAPss4f63QBp6D",
-
-        }
-    })
-        
-    } catch (error: any) {
-        throw new Error(error.message)
-        
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000,
+      });
     }
 
-}
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // true for port 465, false for other ports
+      auth: {
+        user: "maddison53@ethereal.email",
+        pass: "jn7jnAPss4f63QBp6D",
+      },
+    });
+
+    const mailOptions = {
+      from: "arjun@gmail.com",
+      to: email,
+      subject:
+        emailType === "VERIFY" ? "Verify your email" : "Reset your password",
+      html: `<p>Click <a href="${
+        process.env.domain
+      }/verifyemail?token=${hashedToken}">here</a> to ${
+        emailType === "VERIFY" ? "verify your email" : "reset your password"
+      }</p>`,
+    };
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
