992eb1a03faabd5aacb495819d55e21387b38c11
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-16 14:25:41 +0530

    add verifyUserEmail

---
+++ src/app/verifyemail/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import axios from "axios";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export default function verifyEmailPage() {
+  const [token, setToken] = useState("");
+  const [verified, setVerified] = useState(false);
+  const [error, setError] = useState(true);
+
+  const verifyUserEmail = async () => {
+    try {
+      await axios.post("/api/users/verifyemail", { token });
+      setVerified(true);
+    } catch (error: any) {
+      setError(true);
+      console.log(error.response.data);
+    }
+  };
+  useEffect(()=>{
+    if(token.length > 0){
+        verifyUserEmail()
+    }
+  },[])
+}
